2bc5fb0bccbd77fe88a432c65e4b45066dd14822
David Estévez Couñago <a23davidec@teis.intranet>
2025-09-15 12:54:51 +0200

    Formulario centrado

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+from window import *
+
+class Main(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Main, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == '__main__':
+     app = QtWidgets.QApplication(sys.argv)
+     window = Main()
+     window.showMaximized()
+     sys.exit(app.exec())
